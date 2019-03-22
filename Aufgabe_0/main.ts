4fa68846d748a218139e24c2bc7fe81b61e92dbd
Marvin Kübler <Marvin.Kuebler11@web.de>
2019-03-22 12:38:48 +0100

    aufgabe_0

---
+++ Aufgabe_0/main.ts
@@ -0,0 +1,9 @@
+
+function Prompter(){
+  var person= prompt("insert name","name");
+  if (person != null){
+  document.getElementById("Prompt").innerHTML = 
+  "Hello " + person + "hopefully, you'll have a nice day!";
+}
+console.info ("Hallo" + person + "hopefully, you'll have a nice day!")
+}
